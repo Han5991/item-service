18c95b1a68ce9318eadd02c06c51ffcf68b30411
rewq985 <bnbt3@naver.com>
2022-01-01 11:36:29 +0900

    간단한 crud 작업

---
+++ src/main/java/hello/itemservice/ExspringmvcApplication.java
@@ -0,0 +1,13 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExspringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExspringmvcApplication.class, args);
+    }
+
+}
